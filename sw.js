0afc7641faf00750a4dcd493d5a4a6529c5ef9cf
awjcreation <awj.creation@gmail.com>
2026-04-19 15:24:04 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'karta-wzorow-clean-v1';
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./splash.jpg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./images/drgania_fale_mechaniczne_i_swietlne.png",
+  "./images/drgania_fale_mechaniczne_i_swietlne_cd.png",
+  "./images/hydrostatyka_aerostatyka.png",
+  "./images/termodynamika.png",
+  "./images/elektrostatyka.png",
+  "./images/prad_elektryczny.png",
+  "./images/magnetyzm.png",
+  "./images/optyka_geometryczna.png",
+  "./images/elementy_mechaniki_relatywistycznej.png",
+  "./images/elementy_fizyki_atomowej_i_jadrowej.png",
+  "./images/wybrane_zaleznosci.png",
+  "./images/podstawowe_jednostki_ukladu_si.png",
+  "./images/przedrostki_jednostek_miar.png",
+  "./images/wartosci_wybranych_stalych_fizycznych.jpg",
+  "./images/wybrane_stale_i_parametry_astrofizyczne.png",
+  "./images/wartosci_wybranych_jednostek_spoza_ukladu_si.png"
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
