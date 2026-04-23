7e55301f7db4ebb21144f76b3965134bcf1071f7
awjcreation <awj.creation@gmail.com>
2026-04-23 08:55:28 +0200

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,69 +1,75 @@
-const CACHE_NAME = 'karta-wzorow';
+
+const CACHE_NAME = 'karta-wzorow-v1';
 
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './manifest.webmanifest',
-  './splash.jpg',
   './icons/icon-192.png',
   './icons/icon-512.png',
-  './images/drgania_fale_mechaniczne_i_swietlne.png',
-  './images/dynamika.png',
-  './images/elektrostatyka.png',
-  './images/elementy_fizyki_atomowej_i_jadrowej.png',
-  './images/elementy_mechaniki_relatywistycznej.png',
-  './images/grawitacja_i_elementy_astronomii.png',
-  './images/hydrostatyka_aerostatyka.png',
+  './splash.jpg',
   './images/kinematyka.png',
-  './images/magnetyzm.png',
-  './images/optyka_geometryczna.png',
-  './images/podstawowe_jednostki_ukladu_si.png',
-  './images/prad_elektryczny.png',
-  './images/przedrostki_jednostek_miar.png',
+  './images/dynamika.png',
   './images/sily_tarcia_i_sila_sprezystosci.png',
+  './images/grawitacja_i_elementy_astronomii.png',
+  './images/drgania_fale_mechaniczne_i_swietlne.png',
+  './images/optyka_geometryczna.png',
+  './images/hydrostatyka_aerostatyka.png',
   './images/termodynamika.png',
-  './images/wartosci_wybranych_jednostek_spoza_ukladu_si.png',
+  './images/elektrostatyka.png',
+  './images/prad_elektryczny.png',
+  './images/magnetyzm.png',
+  './images/elementy_mechaniki_relatywistycznej.png',
+  './images/elementy_fizyki_atomowej_i_jadrowej.png',
+  './images/wybrane_zaleznosci.png',
+  './images/podstawowe_jednostki_ukladu_si.png',
+  './images/przedrostki_jednostek_miar.png',
   './images/wartosci_wybranych_stalych_fizycznych.png',
   './images/wybrane_stale_i_parametry_astrofizyczne.png',
-  './images/wybrane_zaleznosci.png',
+  './images/wartosci_wybranych_jednostek_spoza_ukladu_si.png',
   './images/uklad_okresowy_pierwiastkow.png'
 ];
 
+// INSTALL
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
+  self.clients.claim();
 });
 
+// FETCH
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+  const request = event.request;
+
+  if (request.method !== 'GET') return;
 
   event.respondWith(
-    caches.match(event.request).then(cached => {
+    caches.match(request).then(cached => {
       if (cached) return cached;
 
-      return fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'));
+      return fetch(request).catch(() => {
+        if (request.destination === 'document') {
+          return caches.match('./index.html');
+        }
+      });
     })
   );
 });
